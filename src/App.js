4a9647539ffcd4db090806ef3a16b3b4a5c0f0a3
Sheharyar Khan <sheharyar.khan1098@gmail.com>
2020-09-25 13:14:58 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -9,15 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <h2> This is sample react app built for picture frame company</h2>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
